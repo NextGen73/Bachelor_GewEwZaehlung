21fd6ec447ad062b8f4ed070bde123f7bf7d95e2
NextGen <noah.goepel@gmail.com>
2024-09-09 23:51:29 +0200

    bug gefixt

--- Programmierung/algorithms.py
+++ Programmierung/algorithms.py
@@ -18,28 +18,30 @@ def  Vorwaertsdifferenz(f, x, h):
     # zuerst wird erste partielle Ableitung approximiert, sollte immer vorhandne sein, solange x ein np.array ist
     xPlusH = x.copy()
     xPlusH[0] += h
-    # result speichert immer eine partielle Ableitung ab
-    result = (f(xPlusH)-f(x))/h
+    # partAbl speichert immer eine partielle Ableitung ab
+    partAbl = (f(xPlusH)-f(x))/h
 
     # falls n>1, dann gibt es mehr als eine partielle Ableitung
     if l>1:
         # solange f ein np.array ausgibt, kann so bestimmt werden, wieviele Dimensionen es besitzt.
         # Sei A der Raum, in den f abbildet, dann ist die ausgegebene Matrix von der Form: A \times IR^l
 
-        dimensionsF = np.ndim(result)
+        dimensionsF = np.ndim(partAbl)
         # hier wird nablaF, welches vorher ein Element in A war, auf ein Element in A \times IR erweitert,
         # damit auch die anderen partiellen Ableitungen geordnet in nablaF gespeichert werden können
-        nablaF = np.expand_dims(result, axis=dimensionsF)
+        nablaF = np.expand_dims(partAbl, axis=dimensionsF)
 
         # wie oben für i=1 wird nun hier die i-te Ableitung bestimmt
         for i in range(1, l):
             xPlusH = x.copy()
             xPlusH[i] += h
-            result = (f(xPlusH)-f(x))/h
-            # result wird wie nablaF oben zu einem Element in A \times IR, um es A \times IR^i hinzuzufügen
+            partAbl = (f(xPlusH)-f(x))/h
+            # partAbl wird wie nablaF oben zu einem Element in A \times IR, um es A \times IR^i hinzuzufügen
             # danach ist nableF ein Element in A \times IR^{i+1}
-            nablaF = np.append(nablaF, np.expand_dims(result, axis=dimensionsF) ,dimensionsF)
+            nablaF = np.append(nablaF, np.expand_dims(partAbl, axis=dimensionsF) ,dimensionsF)
         # nach der Schleife liegt nablaF in der Form A \times IR^l vor, also genau in der Form, wie man es von der Ableitung einer Funktion f:IR^l -> A erwartet
+    else:
+        nablaF = partAbl
     return nablaF
 
 # def  Rueckwaertsdifferenz(f, x, h):
