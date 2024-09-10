fe5ae117496a58d8b2a40516452ef633f308eafe
NextGen <noah.goepel@gmail.com>
2024-09-10 19:52:52 +0200

    Mittelwertdifferenz eingebaut

--- Programmierung/algorithms.py
+++ Programmierung/algorithms.py
@@ -44,18 +44,57 @@ def  Vorwaertsdifferenz(f, x, h):
         nablaF = partAbl
     return nablaF
 
-# def  Rueckwaertsdifferenz(f, x, h):
-#     """
-#     :param f: Funktion, deren Ableitung mithilfe der Rueckwaertsdifferenz approximiert werden soll.
-#     :param x: Wert, an dem Ableitung approximiert werden soll.
-#     :param h: Schrittweite, aufgrund deren der Differenzenquotient gebildet wird.
-#     :raise ValueError: Wenn `h <= 0`.
-#     :return: Approximierung von `f'(x)` mithilfe der Rueckwaertsdifferenz.
-#     """
-#     if(h<=0):
-#         raise ValueError()
+def  Mitteldifferenz(f, x, h):
+    """
+    :param f: Funktion, deren Ableitung mithilfe der Mitteldifferenz approximiert werden soll.
+    :param x: Vektor als np.array, an dem Ableitung approximiert werden soll.
+    :param h: Schrittweite, aufgrund deren der Differenzenquotient gebildet wird.
+    :raise ValueError: Wenn `h <= 0`.
+    :return: Approximierung von `f'(x)` mithilfe der Mitteldifferenz.
+    """
+    # Prüfung, ob h positiv ist
+    if(h<=0):
+        raise ValueError('h muss positiv sein')
     
-#     return (f(x)-f(x-h))/h
+    # Länge des Parameters ermitteln
+    l=len(x)
+
+    # zuerst wird erste partielle Ableitung approximiert, sollte immer vorhandne sein, solange x ein np.array ist
+    xPlusHHalbe = x.copy()
+    xPlusHHalbe[0] += h/2
+
+    xMinusHHalbe = x.copy()
+    xMinusHHalbe[0] -= h/2
+    # partAbl speichert immer eine partielle Ableitung ab
+    partAbl = (f(xPlusHHalbe)-f(xMinusHHalbe))/h
+
+    # falls n>1, dann gibt es mehr als eine partielle Ableitung
+    if l>1:
+        # solange f ein np.array ausgibt, kann so bestimmt werden, wieviele Dimensionen es besitzt.
+        # Sei A der Raum, in den f abbildet, dann ist die ausgegebene Matrix von der Form: A \times IR^l
+
+        dimensionsF = np.ndim(partAbl)
+        # hier wird nablaF, welches vorher ein Element in A war, auf ein Element in A \times IR erweitert,
+        # damit auch die anderen partiellen Ableitungen geordnet in nablaF gespeichert werden können
+        nablaF = np.expand_dims(partAbl, axis=dimensionsF)
+
+        # wie oben für i=1 wird nun hier die i-te Ableitung bestimmt
+        for i in range(1, l):
+            # wie oben nun für i-te part. Abl
+            xPlusHHalbe = x.copy()
+            xPlusHHalbe[i] += h/2
+
+            xMinusHHalbe = x.copy()
+            xMinusHHalbe[i] -= h/2
+
+            partAbl = (f(xPlusHHalbe)-f(xMinusHHalbe))/h
+            # partAbl wird wie nablaF oben zu einem Element in A \times IR, um es A \times IR^i hinzuzufügen
+            # danach ist nableF ein Element in A \times IR^{i+1}
+            nablaF = np.append(nablaF, np.expand_dims(partAbl, axis=dimensionsF) ,dimensionsF)
+        # nach der Schleife liegt nablaF in der Form A \times IR^l vor, also genau in der Form, wie man es von der Ableitung einer Funktion f:IR^l -> A erwartet
+    else:
+        nablaF = partAbl
+    return nablaF
 
 def gradientenverfahren_festeSchrittweite(nablaF, x0, schritt, epsilon):
     """
