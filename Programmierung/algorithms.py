fc68371893053928b3f18fc13e916f3602600956
NextGen <noah.goepel@gmail.com>
2024-09-11 18:43:49 +0200

    das erste Beispiel was geklappt hat

--- Programmierung/algorithms.py
+++ Programmierung/algorithms.py
@@ -1,5 +1,14 @@
 import numpy as np
 
+alpha = 0.1
+schrittweiteDiffverfahren = 0.01
+lamA: float
+lamB: float
+
+def init(start, ende):
+    lamA = start
+    lamB = ende
+
 def  Vorwaertsdifferenz(f, x, h):
     """
     :param f: Funktion, deren Ableitung mithilfe der Vorwaertsdifferenz approximiert werden soll.
@@ -96,26 +105,97 @@ def  Mitteldifferenz(f, x, h):
         nablaF = partAbl
     return nablaF
 
-def gradientenverfahren_festeSchrittweite(nablaF, x0, schritt, epsilon):
+def schrittGradientenverfahren_festeSchrittweite(nablaF, x, schritt):
     """
     :param nableF: Ableitung der Funktion, die minimiert werden soll.
-    :param x0: Startwert für das Gradientenverfahren.
-    :param schritt: Schrittweite, wie weit ein Schritt des Gradientenverfahrens in die berechnete Richtung geht.
-    :param epsilon: Grenze, die den Abbruch des Verfahrens bewirkt, falls `||f'(x_k)|| < epsilon`.
-    :return: Liste der `x_k`, die das Verfahren berechnet hat.
+    :param x: Startwert für das Gradientenverfahren.
+    :param schritt: Schrittweite, wie weit der Schritt des Gradientenverfahrens in die berechnete Richtung geht.
+    :return: berechnete neue Stelle `x`.
     """
 
-    xArray = np.array([x0])
-    i=0
-    x_alt = x0
-    while np.norm(nablaF(xArray[i]))>=epsilon:
-        x_neu = xArray[i]-schritt*nablaF(xArray[i])
+    abl_real = nablaF(x).real
+    return x-schritt*abl_real
 
-        np.append(xArray, x_neu)
-        x_alt = x_neu
-        i+=1
+# das beruht auf echter Trapezregel, wo man Mittelwert bildet und Differenz der Stellen
+def quadratureContourIntegralCircleTrapez(f, gamma:complex, r:float, m:int, s) -> complex:
+    # Berechne Stützstellen
+    zs = np.array([gamma+r*np.exp(2*np.pi*1j/m*(k+1/2)) for k in range(m+1)])
+    return sum((f(zs[i+1], s)+f(zs[i], s))/2*(zs[i+1]-zs[i]) for i in range(m))
 
-        if(i>=10000):
-            break
+# das ist eher die Mittelpunktsregel, wo man Funktion nur an einer Stelle auswerten muss, Differenz wurde explizit berechnet und rausgezogen
+def quadratureContourIntegralCircleMittelpunkt(f, gamma:complex, r:float, m:int, s) -> complex:
+    # Berechne Stützstellen
+    zs = np.array([gamma+r*np.exp(2*np.pi*1j/m*(k+1/2)) for k in range(m)])
+    return sum(f(zs[i], s)*np.exp(2*np.pi*1j*i/m) for i in range(m))*r*(np.exp(2*np.pi*1j/m)-1)
 
-    return xArray
+# minimiert die gewichtete Zaehlung der Eigenwerte, berechnet tatsächliche gewichtete Eigenwert-Zaehlung, gibt alles aus
+def EigenwerteMinimierenAufIntervall(M:np.ndarray, K:np.ndarray, startpunkt:np.ndarray, lambda_a, lambda_b, anzahlStuetzstellen:int, schrittweiteGradVerf:float, fehlertoleranz:float) -> np.ndarray:
+    """
+    :param M: Massematrix.
+    :param K: Steifigkeitsmatrix.
+    :param lambda_a: untere Grenze des Intervalls.
+    :param lambda_b: obere Grenze des Intervalls.
+    :anzahlStuetzstellen: Anzahl der Stützstellen in Quadraturformel.
+    :param schrittweiteGradVerf: Schrittweite, wie weit ein Schritt des Gradientenverfahrens in die berechnete Richtung geht.
+    :param fehlertoleranz: Grenze, wenn J(s) drunter fällt, dann wird Minimierungsverfahren beendet.
+    :return: Vektor, 1. Spalte: berechneten Werte s, 2. Spalte: gewichtete Zählung (genau), 3. Spalte: gewichtete Zählung (approximiert).
+    """
+
+    z0Tilde = (lambda_a+lambda_b)/2
+    rTilde = (lambda_b-lambda_a)/2
+    len_s = len(startpunkt)
+
+    # Gewichtungsfunktion g(z)
+    def g(z: complex) -> complex:
+        return -(z-((1+alpha)*lambda_a-alpha*lambda_b))*(z-((1+alpha)*lambda_b-alpha*lambda_a))
+    
+    # berechnet die Eigenwerte mittels funktion np.linalg.eigvals, siehe Definition mu
+    def gewichteteEWZaehlung_genau(s)->float:
+        # ist \1_{[\lambda_a,\lambda_b]}(z)
+        def h(z:float)->float:
+            if(z < lambda_b and z>lambda_a):
+                return 1.
+            return 0.
+        
+        eigvals = np.linalg.eigvals(np.linalg.inv(M(s)).dot(K(s)))
+        return sum(h(i)*g(i) for i in eigvals)
+
+    def J_Stern(s)->float:
+        # zur besseren Lesbarkeit wird B = (zM-K)^{-1} M ausgelagert
+        def B(z:complex, s:np.ndarray)-> np.ndarray[complex]:
+            D = np.linalg.inv(z*M(s)-K(s))
+            return g(z)*np.trace(D.dot(M(s)))
+        
+        return quadratureContourIntegralCircleTrapez(B, z0Tilde, rTilde, anzahlStuetzstellen, s)/2/np.pi/1j
+    
+    def nablaJ_Stern(s)->float:
+        def nablaB(z:complex, s:np.ndarray)->np.ndarray[complex]:
+            D = np.linalg.inv(z*M(s)-K(s))
+            dMds = Vorwaertsdifferenz(M, s, schrittweiteDiffverfahren)
+            dKds = Vorwaertsdifferenz(K, s, schrittweiteDiffverfahren)
+
+            return g(z)*(np.trace(D.dot(dMds))-np.trace(D.dot(z*dMds-dKds).dot(D)))
+        return quadratureContourIntegralCircleTrapez(nablaB, z0Tilde, rTilde, anzahlStuetzstellen, s)/2/np.pi/1j
+
+################## hier wird in jedem Schritt mehrmals M(s) und K(s) berechnet ##################
+
+    # result wird eine 2d-Matrix, result[0:j-1,i] ist Wert, der in i-tem Schritt berechnet wurde
+    # result[-2,i] genaue gewichtete Zaehlung der Eigenwerte mit Wert aus i-tem Schritt
+    # result[-1,i] approximierte gewichtete Zaehlung der Eigenwerte mit Wert aus i-tem Schritt
+
+    result = np.expand_dims(np.append(startpunkt, [gewichteteEWZaehlung_genau(startpunkt), J_Stern(startpunkt)]), 1)
+
+    while result[-1,-1]>=fehlertoleranz:
+        x_neu = schrittGradientenverfahren_festeSchrittweite(nablaJ_Stern, np.array(result[0:len_s,-1]), schrittweiteGradVerf)
+
+        result = np.append(result, np.expand_dims(np.append(x_neu, [gewichteteEWZaehlung_genau(x_neu), J_Stern(x_neu)]), 1), axis=1)
+
+        if(np.size(result,1)%100 == 0):
+            print(result[-2,-1])
+            if(np.size(result,1) == 500):
+                break
+
+
+        
+
+    return result
--- Programmierung/test.py
+++ Programmierung/test.py
@@ -1,81 +1,55 @@
 import numpy as np
 import matplotlib.pyplot as plots
 import algorithms
-import cmath
 
 lambda_a = 0.5
 lambda_b = 1.5
-alpha = 0.1
 n: int = 8
-s = np.array([3])
-m = 1000
-h = 0.01
-z0Tilde = (lambda_a+lambda_b)/2
-rTilde = (lambda_b-lambda_a)/2
-
-phis = np.array([2*np.pi/m*(k+1/2) for k in range(m)])
-# zs = np.array([z0Tilde+cmath.rect(rTilde, phi) for phi in phis])
-zs = np.array([z0Tilde+rTilde*np.exp(2*np.pi*complex(0,1)/m*(k+1/2)) for k in range(m)])
-
-cs=(zs-z0Tilde)/m
+s = np.array([8])
+m = 100
 
 def K(s: float)->np.ndarray:
     return K_System1(s, 1.3, 3)
 
 def M(s: float)->np.ndarray:
-    return M_System1(3, 4, 1)
+    return M_System1(s, 3, 4, 1)
 
-def K_System1(s, c, j):
+def K_System1(s:np.ndarray, c, j:int):
 
     result = np.zeros((n,n))
 
-    diag=np.zeros(n)
-    diag[range(j)] = 2*s
-    diag[j] = s+c
-    diag[range(j+1,n)] = 2*c 
+    diag=np.ones(n)*s[0].real
+    diag[range(j)] += 2
+    diag[j]+=c
+    diag[range(j+1,n)] += 2*c 
 
     np.fill_diagonal(result, diag)
 
-    nebendiag = np.zeros(n-1)
-    nebendiag[range(j)] = -s
-    nebendiag[range(j,n-1)] = -c
+    nebendiag = np.ones(n-1)*-s[0].real
+    nebendiag[range(j,n-1)] += -c
 
     result += np.diag(nebendiag, 1)
     result += np.diag(nebendiag, -1)
     return result
 
-def M_System1(m1, m2, j):
+def M_System1(s:np.ndarray, m1, m2, j):
 
     result = np.zeros((n,n))
-    diag = np.append(np.full(j-1, m1), np.full(n-j+1, m2))
+    diag = np.append(np.full(j-1, m1*s[0]), np.full(n-j+1, m2*s[0]))
     np.fill_diagonal(result, diag)
     return result
 
-def D(s, z)-> np.ndarray:
-    C = z*M(s)-K(s)
-    return np.linalg.inv(C)
+result = algorithms.EigenwerteMinimierenAufIntervall(M, K, s, lambda_a, lambda_b, m, 0.5, 1e-6)
 
-def g(z: complex) -> complex:
-    return -(z-((1+alpha)*lambda_a-alpha*lambda_b))*(z-((1+alpha)*lambda_b-alpha*lambda_a))
+s = result[0:len(s),:]
+EWgenau = result[-2,:]
+EWapprox = result[-1,:]
 
-# print(M(np.array([1,1])))
-# x = np.array([0.0,0.0])
-# print(np.trace(algorithms.Vorwaertsdifferenz(K, x, 0.1)))
-# print(algorithms.Vorwaertsdifferenz(K, s, 0.001))
+schritte = len(EWapprox)
 
-def JStern(s):
-    return sum(g(zs[k])*np.trace(D(s, zs[k]).dot(M(s)))*cs[k] for k in range(m))/2/np.pi/complex(0,1)
+plots.plot(range(schritte), EWgenau, label="genau")
+# plots.plot(range(schritte), s.real, label="s")
+plots.show()
 
-def nablaJStern(s):
-    result = 0
-    for k in range(m):
-        Dk = D(s, zs[k])
-        dMds = algorithms.Vorwaertsdifferenz(M, s, h)
-        dKds = algorithms.Vorwaertsdifferenz(K, s, h)
-        result+=g(zs[k])*(np.trace(Dk.dot(dMds))-np.trace(Dk.dot(zs[k]*dMds-dKds).dot(Dk)))*cs[k]
-    return result/2/np.pi/complex(0,1)
+print(np.linalg.eigvals(np.linalg.inv(M(s[-1])).dot(K(s[-1]))))
 
-# print(JStern(s))
-print(np.linalg.eigvals(np.linalg.inv(M(s)).dot(K(s))))
-k = np.array([0.7581948, 1.19455199, 0.98672309])
-print(sum(g(z) for z in k))
