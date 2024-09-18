8cdd18c3f672a33f2aa898932aa422978027fa1e
NextGen <noah.goepel@gmail.com>
2024-09-18 14:57:28 +0200

    nur m=1 benötigt

--- Programmierung/Verbesserung_NablaJ.py
+++ Programmierung/Verbesserung_NablaJ.py
@@ -13,27 +13,21 @@ if(__name__=='__main__'):
     # beachte, dass man immer init() und systemAuswaehlen() aufrufen muss
     # ohne diese Aufrufe koennen keine Berechnungen durchgeführt werden
     
-    init(algorithms.quadratureContourIntegralCircleGaussZwei, n, j)
+    init(algorithms.quadratureContourIntegralCircleGaussZwei, n, j, maxIter=100000)
     print("Da die Quadratur nicht mehr so empfindlich gegenüber Eigenwerten nahe der Integrationskurve ist, macht eine Quadraturformel mit mehr Teilintervallen kaum einen Unterschied mehr.")
     print("Alle folgenden Durchläufe werden mit der verschobenen Trapezformel durchgeführt.")
     print("Die ersten zwei sind wieder System 1 mit kleiner Schrittweite des Gradientenverfahrens, beim zweiten Plot wurde die Integrationskurve in mehr Teilintervalle aufgeteilt.\n")
     # System 1 mit Standardwerten initialisieren
     systemAuswaehlen(1)
 
-    minimierenPlottenUndEckdatenAnzeigen(100, 0.05, approxNablaJ=True)
-    # Verwendung von mehr Teilintervallen
-    minimierenPlottenUndEckdatenAnzeigen(150, 0.05, approxNablaJ=True)
+    minimierenPlottenUndEckdatenAnzeigen(1, 0.05, approxNablaJ=True)
 
     print("Erhöhe nun die Schrittweite des Gradientenverfahrens, um eine schnellere Konvergenz zu erwarten.")
     # Verfahren mit Standardwerten, aber groesserer Schrittweite des Gradientenverfahrens
-    minimierenPlottenUndEckdatenAnzeigen(100, 0.5, approxNablaJ=True)
-    # wieder mehr Teilintervalle verwenden
-    minimierenPlottenUndEckdatenAnzeigen(150, 0.5, approxNablaJ=True)
+    minimierenPlottenUndEckdatenAnzeigen(1, 0.5, approxNablaJ=True)
 
     print("Betrachte nun das zweite System mit kleiner Schrittweite.")
     # System 2 mit Standardwerten
     systemAuswaehlen(2)
 
-    minimierenPlottenUndEckdatenAnzeigen(100, 0.05, approxNablaJ=True)
-    # mehr Teilintervalle verwenden
-    minimierenPlottenUndEckdatenAnzeigen(150, 0.05, approxNablaJ=True)
+    minimierenPlottenUndEckdatenAnzeigen(1, 0.05, approxNablaJ=True)
